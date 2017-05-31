b79f1f495610be11dd2a27a2f9e4c723fe7479ca
jiangyong <jiangyong@didichuxing.com>
2017-05-31 16:47:00 +0800

    file

--- sys/cmd.go
+++ sys/cmd.go
@@ -39,30 +39,3 @@ func CmdOutTimeout(timeout time.Duration, name string, arg ...string) (string, e
 		return out.String(), err
 	}
 }
-
-func CmdOutTimeout1(cmd *exec.Cmd, timeout time.Duration) (error, bool) {
-
-	done := make(chan error)
-	go func() {
-		done <- cmd.Wait()
-	}()
-
-	var err error
-	select {
-	case <-time.After(timeout):
-		log.Printf("timeout, process:%s will be killed", cmd.Path)
-
-		go func() {
-			<-done // allow goroutine to exit
-		}()
-
-		// timeout
-		if err = cmd.Process.Kill(); err != nil {
-			log.Printf("failed to kill: %s, error: %s", cmd.Path, err)
-		}
-
-		return err, true
-	case err = <-done:
-		return err, false
-	}
-}
